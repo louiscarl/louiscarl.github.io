606504645bfa4a6a496742caa3faef084438145d
Louis Carl Pepin <p.louis4@gmail.com>
2015-04-12 15:13:53 -0700

    Added textures.

--- AsteroLens/app.ts
+++ AsteroLens/app.ts
@@ -1,77 +1,81 @@
-var __extends = this.__extends || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    function __() { this.constructor = d; }
-    __.prototype = b.prototype;
-    d.prototype = new __();
-};
-var SolarSystem = (function () {
-    function SolarSystem() {
-        var _this = this;
-        $.getJSON('planets.js', function (data) {
-            _this.system = data;
+﻿declare var math: any;
+declare var $: any;
+
+class SolarSystem {
+    system: any;
+
+    constructor() {
+        $.getJSON('planets.js', (data) => {
+            this.system = data;
         });
     }
-    return SolarSystem;
-})();
-var AsteroMath = (function () {
-    function AsteroMath() {
-    }
-    AsteroMath.r3 = function (x) {
+}
+
+class AsteroMath {
+    static r3(x: number) {
         return math.matrix([
             [Math.cos(x), Math.sin(x), 0],
             [-Math.sin(x), Math.cos(x), 0],
             [0, 0, 1]
         ]);
-    };
-    AsteroMath.r1 = function (x) {
+    }
+
+    static r1(x: number) {
         return math.matrix([
             [1, 0, 0],
             [0, Math.cos(x), Math.sin(x)],
             [0, -Math.sin(x), Math.cos(x)]
         ]);
-    };
-    AsteroMath.auToKm = function (dist) {
+    }
+
+    static auToKm(dist) {
         return dist * 149579871;
-    };
-    AsteroMath.kmToAu = function (dist) {
+    }
+
+    static kmToAu(dist) {
         return dist / 149579871;
-    };
-    AsteroMath.degToRad = function (angle) {
+    }
+
+    static degToRad(angle) {
         return angle / 180 * Math.PI;
-    };
-    AsteroMath.radToDeg = function (angle) {
+    }
+
+    static radToDeg(angle) {
         return angle / Math.PI * 180;
-    };
-    AsteroMath.toCartesian = function (a, ecc, inc, Omega, w, nu, U) {
+    }
+
+    static toCartesian(a: number, ecc: number, inc: number, Omega: number, w: number, nu: number, U: number) {
         var p = a * ((1 - ecc) ^ 2);
         var r_pqw1 = p * Math.cos(nu) / (1 + ecc * Math.cos(nu));
         var r_pqw2 = p * Math.sin(nu) / (1 + ecc * Math.cos(nu));
         var r_pqw3 = 0;
+
         var r_pqw = math.matrix([r_pqw1, r_pqw2, r_pqw3]);
         var ra = math.multiply(AsteroMath.r3(-Omega), AsteroMath.r1(-inc));
         var rb = math.multiply(ra, AsteroMath.r3(-w));
         var rc = math.multiply(rb, r_pqw);
+
         return rc;
-    };
-    AsteroMath.pythagoras = function (m) {
+    }
+
+    static pythagoras(m: number[]) {
         return Math.sqrt(m[0] * m[0] + m[1] * m[1] + m[2] * m[2]);
-    };
-    AsteroMath.sphericalToCartesian = function (lat, lon, r) {
+    }
+
+    static sphericalToCartesian(lat: number, lon: number, r: number) {
         var latRad = AsteroMath.degToRad(lat);
         var lonRad = AsteroMath.degToRad(lon);
+
         var x = r * Math.cos(latRad) * Math.cos(lonRad);
         var y = r * Math.cos(latRad) * Math.sin(lonRad);
         var z = r * Math.sin(latRad);
+
         return [x, y, z];
-    };
-    return AsteroMath;
-})();
-var AsteroMathTest = (function (_super) {
-    __extends(AsteroMathTest, _super);
-    function AsteroMathTest() {
-        _super.apply(this, arguments);
     }
-    AsteroMathTest.prototype.r1Test = function () {
+}
+
+class AsteroMathTest extends AsteroMath {
+    r1Test() {
         console.log(AsteroMath.r1(Math.PI / 4));
         /*var expected = math.matrix(
             [
@@ -80,8 +84,9 @@ var AsteroMathTest = (function (_super) {
                 [0, -0.7071, 0.7071]
             ]);
         */
-    };
-    AsteroMathTest.prototype.r3Test = function () {
+    }
+
+    r3Test() {
         console.log(AsteroMath.r3(Math.PI / 4));
         /* var expected = math.matrix(
             [
@@ -90,8 +95,9 @@ var AsteroMathTest = (function (_super) {
                 [0, 0, 1]
             ]);
          */
-    };
-    AsteroMathTest.prototype.sphericalToCartesianTest = function () {
+    }
+
+    sphericalToCartesianTest() {
         var mars = AsteroMath.sphericalToCartesian(48.7687, -0.0240, 1.469496318582);
         var marsDist = AsteroMath.pythagoras(mars);
         var expected = 1.469496318582;
@@ -100,43 +106,61 @@ var AsteroMathTest = (function (_super) {
             console.error('result: ' + marsDist);
             console.error('expected: ' + expected);
         }
-    };
-    AsteroMathTest.prototype.run = function () {
+    }
+
+    run() {
         //this.r1Test();
         //this.r3Test();
         this.sphericalToCartesianTest();
-    };
-    return AsteroMathTest;
-})(AsteroMath);
-var Renderer = (function () {
-    function Renderer() {
-        this.createScene = function () {
-            this.scene = new BABYLON.Scene(this.engine);
-            this.scene.clearColor = new BABYLON.Color3(0.1, 0.1, 0.1);
-            var camera = new BABYLON.OculusCamera("camera1", new BABYLON.Vector3(0, 0, 0), this.scene);
-            //var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 0, 0), this.scene);
-            camera.setTarget(new BABYLON.Vector3(0, 1, 0));
-            camera.attachControl(this.canvas, true);
-            var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), this.scene);
-            light.intensity = 0.7;
-            this.system = new SolarSystem();
-            return this.scene;
-        };
-        this.canvas = document.getElementById("renderCanvas");
+    }
+}
+
+class Renderer {
+    canvas: HTMLCanvasElement;
+    engine: BABYLON.Engine;
+    scene: BABYLON.Scene;
+    system: SolarSystem;
+
+    constructor() {
+        this.canvas = <HTMLCanvasElement>document.getElementById("renderCanvas");
         this.engine = new BABYLON.Engine(this.canvas, true);
     }
-    Renderer.prototype.updateScene = function () {
+
+    createScene = function () {
+        this.scene = new BABYLON.Scene(this.engine);
+
+        this.scene.clearColor = new BABYLON.Color3(0.1, 0.1, 0.1);
+        
+        var camera = new BABYLON.OculusCamera("camera1", new BABYLON.Vector3(0, 0, 0), this.scene);
+        //var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 0, 0), this.scene);
+        camera.setTarget(new BABYLON.Vector3(0, 1, 0));
+        
+        camera.attachControl(this.canvas, true);
+        
+        var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), this.scene);
+        light.intensity = 0.7;
+
+        this.system = new SolarSystem();
+
+        return this.scene;
+    }
+
+    updateScene() {
         var s = this.system.system;
+
         var sun = BABYLON.Mesh.CreateSphere('sun', 4, 0.5, this.scene);
         var sun_mat = new BABYLON.StandardMaterial("texture1", this.scene);
         sun_mat.emissiveColor = new BABYLON.Color3(1, 1, 0);
+        
         sun.material = sun_mat;
         sun.position.x = 0;
         sun.position.y = 0;
         sun.position.z = 0;
+        
         for (var i = 0; i < s.length; ++i) {
             if (s[i].name) {
                 var sphere = BABYLON.Mesh.CreateSphere('p' + i, 10, 0.2, this.scene);
+
                 var mat = new BABYLON.StandardMaterial('t' + i, this.scene);
                 mat.emissiveColor = new BABYLON.Color3(s[i].cr / 255, s[i].cg / 255, s[i].cb / 255);
                 if (s[i].img) {
@@ -150,15 +174,19 @@ var Renderer = (function () {
                 sphere.position.z = s[i].z;
             }
         }
+
         var material = new BABYLON.StandardMaterial("texture1", this.scene);
         material.emissiveColor = new BABYLON.Color3(1, 1, 1);
         //material.diffuseColor = new BABYLON.Color3(1.0, 0.2, 0.7);
-        var randomCoordinate = function (x) {
-            var v = [Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5];
+
+        var randomCoordinate = (x) => {
+            var v =[Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5];
             var d = AsteroMath.pythagoras(v);
+
             var nx = (Math.random() + x) / d;
             return [v[0] * nx, v[1] * nx, v[2] * nx];
         };
+
         for (var i = 0; i < 100; ++i) {
             var sphere = BABYLON.Mesh.CreateSphere('p' + i, 4, 0.05, this.scene);
             sphere.material = material;
@@ -167,6 +195,7 @@ var Renderer = (function () {
             sphere.position.y = c[1];
             sphere.position.z = c[2];
         }
+
         for (var i = 0; i < 100; ++i) {
             var sphere = BABYLON.Mesh.CreateSphere('p' + i, 4, 0.05, this.scene);
             sphere.material = material;
@@ -175,36 +204,46 @@ var Renderer = (function () {
             sphere.position.y = c[1];
             sphere.position.z = c[2];
         }
+
         document.getElementById('body-name').innerText = '';
-    };
-    Renderer.prototype.start = function () {
-        var _this = this;
+    }
+
+    start() {
         this.createScene();
-        this.engine.runRenderLoop(function () {
-            _this.scene.render();
+
+        this.engine.runRenderLoop(() => {
+            this.scene.render();
         });
+
         // Resize
         //window.addEventListener("resize",() => {
         //    this.engine.resize();
         //});
-    };
-    Renderer.prototype.next = function () {
+    }
+
+    lookAtIndex: number;
+
+    next() {
         var s = this.system.system;
         if (this.lookAtIndex === undefined) {
             this.lookAtIndex = s.length - 1;
         }
-        var cam = this.scene.activeCamera;
+        
+        var cam = <BABYLON.OculusCamera>this.scene.activeCamera;
+
         var i = this.lookAtIndex;
+
         cam.setTarget(new BABYLON.Vector3(s[i].x, s[i].y, s[i].z));
         document.getElementById('body-name').innerText = s[i].name;
+
         this.lookAtIndex = i - 1;
         if (this.lookAtIndex < 0) {
             this.lookAtIndex = s.length - 1;
         }
-    };
-    return Renderer;
-})();
-window.onload = function () {
+    }
+}
+
+window.onload = () => {
     /*
     var r = AsteroMath.toCartesian(
         AsteroMath.auToKm(1.458),
@@ -221,45 +260,44 @@ window.onload = function () {
 
     console.log(AsteroMath.pythagoras(rAu));
     */
+
     var mars = AsteroMath.sphericalToCartesian(48.7687, -0.0240, 1.469496318582);
     console.log(mars);
     console.log(AsteroMath.pythagoras(mars));
+
     var test = new AsteroMathTest();
     test.run();
+
     var ren = new Renderer();
     ren.start();
+
     setTimeout(ren.updateScene.bind(ren), 1000);
+
     document.addEventListener('keyup', ren.next.bind(ren));
 };
+
 function launchFullscreen(element) {
-    var document = window.document;
-    if (!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {
+    var document: any = window.document;
+    if (!document.fullscreenElement &&    // alternative standard method
+        !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {  // current working methods
         if (document.documentElement.requestFullscreen) {
             document.documentElement.requestFullscreen();
-        }
-        else if (document.documentElement.msRequestFullscreen) {
+        } else if (document.documentElement.msRequestFullscreen) {
             document.documentElement.msRequestFullscreen();
-        }
-        else if (document.documentElement.mozRequestFullScreen) {
+        } else if (document.documentElement.mozRequestFullScreen) {
             document.documentElement.mozRequestFullScreen();
-        }
-        else if (document.documentElement.webkitRequestFullscreen) {
+        } else if (document.documentElement.webkitRequestFullscreen) {
             document.documentElement.webkitRequestFullscreen();
         }
-    }
-    else {
+    } else {
         if (document.exitFullscreen) {
             document.exitFullscreen();
-        }
-        else if (document.msExitFullscreen) {
+        } else if (document.msExitFullscreen) {
             document.msExitFullscreen();
-        }
-        else if (document.mozCancelFullScreen) {
+        } else if (document.mozCancelFullScreen) {
             document.mozCancelFullScreen();
-        }
-        else if (document.webkitExitFullscreen) {
+        } else if (document.webkitExitFullscreen) {
             document.webkitExitFullscreen();
         }
     }
 }
-//# sourceMappingURL=app.js.map
